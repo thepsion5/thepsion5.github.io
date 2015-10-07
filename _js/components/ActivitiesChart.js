83a1c84f3833acc9f32a90d38eb3ce5c154a178d
unknown <IE21SSM@IE1819WM09449.net.ads.state.tn.us>
2015-10-06 20:20:26 -0500

    Behold, A CHART!

--- _js/components/ActivitiesChart.js
+++ _js/components/ActivitiesChart.js
@@ -14,16 +14,94 @@ class ActivitiesChart extends React.Component
         this.initializeChart();
     }
 
+    //TODO: Break labels into two lines when they would collide with the donut
+    static getReadableLabel(value, label)
+    {
+        var readableValue = value + ' hours';
+        if(value < 1) {
+            readableValue = value * 60 + ' minutes';
+        } else if(value == 1) {
+            readableValue = '1 hour';
+        }
+        readableValue += "\n" + label[0].textContent;
+
+        return readableValue;
+    }
+
+
+
     initializeChart()
     {
-        console.log(this.props);
+        //Inspired by and borrowing heavily from: http://codepen.io/dshapira/pen/CJind
+        //TODO: Re-render the chart on resize
         var $container = $(this.props.container);
-        var containerHeight = parseInt($container.css('height')),
-            containerWidth  = parseInt($container.css('width')),
-            radius  = Math.min(containerWidth, containerHeight) / 3,
-            backgroundColor = this.props.background_color,
+        var backgroundColor = this.props.background_color,
+            strokeColor = this.props.stroke_color,
             data = this.generateChartData();
-        //TODO: Actually generate the chart here
+
+        var dimensions = this.getChartDimensions($container, data);
+        $container.css('height', dimensions.container.height + 'px');
+        var paper = new Raphael($container[0], dimensions.container.width, dimensions.container.height);
+
+        //Render the Pie Chart
+        var pie = paper.piechart(dimensions.chart.x, dimensions.chart.y, dimensions.chart.radius, data.values, {
+            legend: data.labels,
+            legendpos: 'south',
+            legendcolor: strokeColor,
+            stroke: strokeColor,
+            strokewidth: 1,
+            colors: data.colors
+        });
+
+        //Handle hover in and out
+        pie.hover(function () {
+            this.sector.stop();
+            this.sector.scale(1.1, 1.1, this.cx, this.cy);
+
+            if (this.label) {
+                this.label[0].stop();
+                this.label[0].attr({ r: 8.5 });
+                this.label[1].attr({ "font-weight": 800 });
+                centerLabel.stop();
+                centerLabel.attr('text', ActivitiesChart.getReadableLabel(this.value.value, this.label[1]));
+                centerLabel.animate({ 'opacity': 1.0 }, 200);
+            }
+        }, function () {
+            this.sector.animate({ transform: 's1 1 ' + this.cx + ' ' + this.cy }, 500, "bounce");
+            this.sector.animate({ 'stroke': strokeColor }, 400);
+            if (this.label) {
+                this.label[0].attr({ r: 5 });
+                this.label[1].attr({ "font-weight": 400 });
+                centerLabel.stop();
+                centerLabel.animate({ 'opacity': 0.0 }, 500);
+            }
+        });
+
+        //Render the inner circle that turns the chart from Pie to Donut (delicious)
+        paper.circle(dimensions.chart.x, dimensions.chart.y, dimensions.chart.inner_radius)
+            .attr({'fill': backgroundColor, 'stroke': strokeColor});
+
+        //Render the center label
+        var centerLabel = paper.text(dimensions.chart.x, dimensions.chart.y, '')
+            .attr({'fill': strokeColor, 'font-size': 16, "font-weight": 800, 'opacity': 0.0 });
+    }
+
+    getChartDimensions($container, chartData)
+    {
+        var labelOffset = 14 * chartData.values.length;
+        var baseWidth = parseInt($container.css('width'));
+        return {
+            'chart' : {
+                'x' : baseWidth / 2,
+                'y' : baseWidth / 2,
+                'radius' : baseWidth / 3,
+                'inner_radius' : (baseWidth / 3) * 0.6
+            },
+            'container' : {
+                'width' : baseWidth,
+                'height' : baseWidth + 10 + labelOffset
+            }
+        };
     }
 
     generateChartData()
@@ -49,7 +127,7 @@ class ActivitiesChart extends React.Component
         return (
             <div className="col-sm-6">
                 <h3>My Average Day</h3>
-                <div id="activities-chart">Visualize this chart mentally until I actually do it.</div>
+                <div id="activities-chart" />
             </div>
         );
     }
--- js/bundle.js
+++ js/bundle.js
@@ -47,17 +47,79 @@ var ActivitiesChart = (function (_React$Component) {
         value: function componentDidMount() {
             this.initializeChart();
         }
+
+        //TODO: Break labels into two lines when they would collide with the donut
     }, {
         key: 'initializeChart',
         value: function initializeChart() {
-            console.log(this.props);
+            //Inspired by and borrowing heavily from: http://codepen.io/dshapira/pen/CJind
+            //TODO: Re-render the chart on resize
             var $container = $(this.props.container);
-            var containerHeight = parseInt($container.css('height')),
-                containerWidth = parseInt($container.css('width')),
-                radius = Math.min(containerWidth, containerHeight) / 3,
-                backgroundColor = this.props.background_color,
+            var backgroundColor = this.props.background_color,
+                strokeColor = this.props.stroke_color,
                 data = this.generateChartData();
-            //TODO: Actually generate the chart here
+
+            var dimensions = this.getChartDimensions($container, data);
+            $container.css('height', dimensions.container.height + 'px');
+            var paper = new Raphael($container[0], dimensions.container.width, dimensions.container.height);
+
+            //Render the Pie Chart
+            var pie = paper.piechart(dimensions.chart.x, dimensions.chart.y, dimensions.chart.radius, data.values, {
+                legend: data.labels,
+                legendpos: 'south',
+                legendcolor: strokeColor,
+                stroke: strokeColor,
+                strokewidth: 1,
+                colors: data.colors
+            });
+
+            //Handle hover in and out
+            pie.hover(function () {
+                this.sector.stop();
+                this.sector.scale(1.1, 1.1, this.cx, this.cy);
+
+                if (this.label) {
+                    this.label[0].stop();
+                    this.label[0].attr({ r: 8.5 });
+                    this.label[1].attr({ "font-weight": 800 });
+                    centerLabel.stop();
+                    centerLabel.attr('text', ActivitiesChart.getReadableLabel(this.value.value, this.label[1]));
+                    centerLabel.animate({ 'opacity': 1.0 }, 200);
+                }
+            }, function () {
+                this.sector.animate({ transform: 's1 1 ' + this.cx + ' ' + this.cy }, 500, "bounce");
+                this.sector.animate({ 'stroke': strokeColor }, 400);
+                if (this.label) {
+                    this.label[0].attr({ r: 5 });
+                    this.label[1].attr({ "font-weight": 400 });
+                    centerLabel.stop();
+                    centerLabel.animate({ 'opacity': 0.0 }, 500);
+                }
+            });
+
+            //Render the inner circle that turns the chart from Pie to Donut (delicious)
+            paper.circle(dimensions.chart.x, dimensions.chart.y, dimensions.chart.inner_radius).attr({ 'fill': backgroundColor, 'stroke': strokeColor });
+
+            //Render the center label
+            var centerLabel = paper.text(dimensions.chart.x, dimensions.chart.y, '').attr({ 'fill': strokeColor, 'font-size': 16, "font-weight": 800, 'opacity': 0.0 });
+        }
+    }, {
+        key: 'getChartDimensions',
+        value: function getChartDimensions($container, chartData) {
+            var labelOffset = 14 * chartData.values.length;
+            var baseWidth = parseInt($container.css('width'));
+            return {
+                'chart': {
+                    'x': baseWidth / 2,
+                    'y': baseWidth / 2,
+                    'radius': baseWidth / 3,
+                    'inner_radius': baseWidth / 3 * 0.6
+                },
+                'container': {
+                    'width': baseWidth,
+                    'height': baseWidth + 10 + labelOffset
+                }
+            };
         }
     }, {
         key: 'generateChartData',
@@ -68,18 +130,11 @@ var ActivitiesChart = (function (_React$Component) {
                 "colors": []
             };
 
+            var colorsByCategory = this.props.label_colors;
             this.props.activities.forEach(function (activity) {
                 chartData.values.push(activity.hours);
                 chartData.labels.push(activity.activity);
-                var color = '';
-                switch (activity.category) {
-                    case 'professional':
-                        color = "#99F";break;
-                    case 'personal':
-                        color = "#F99";break;
-                    case 'sleep':
-                        color = "#9F9";break;
-                }
+                var color = colorsByCategory[activity.category] ? colorsByCategory[activity.category] : colorsByCategory['default'];
                 chartData.colors.push(color);
             });
             return chartData;
@@ -98,6 +153,19 @@ var ActivitiesChart = (function (_React$Component) {
                 React.createElement('div', { id: 'activities-chart' })
             );
         }
+    }], [{
+        key: 'getReadableLabel',
+        value: function getReadableLabel(value, label) {
+            var readableValue = value + ' hours';
+            if (value < 1) {
+                readableValue = value * 60 + ' minutes';
+            } else if (value == 1) {
+                readableValue = '1 hour';
+            }
+            readableValue += "\n" + label[0].textContent;
+
+            return readableValue;
+        }
     }]);
 
     return ActivitiesChart;
@@ -108,7 +176,13 @@ ActivitiesChart.defaultProps = {
     "container": "#activities-chart",
     "donut_size": 0.5,
     "background_color": "#FFF",
-    "colors": ["#AAF", "#AFF", "#FAA"]
+    "stroke_color": "#000",
+    "label_colors": {
+        "professional": "#FAA",
+        "personal": "#AAF",
+        "sleep": "#AFA",
+        "default": "#AAA"
+    }
 };
 
 exports['default'] = ActivitiesChart;
